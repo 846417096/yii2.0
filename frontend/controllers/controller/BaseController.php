87e31e86c3b8ca4c439939e504c823f2075e33fc
zzx <846417096@qq.com>
2017-08-09 14:34:42 +0800

    8/9
    新增一个控制器

---
+++ frontend/controllers/controller/BaseController.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 84641
+ * Date: 2017/8/7
+ * Time: 0:58
+ */
+
+namespace frontend\controllers\controller;
+
+
+use yii\web\Controller;
+use common\models\HttpRequest;
+
+class BaseController extends Controller
+{
+    /*
+     *@param $url 请求地址
+     *@return  返回json
+     */
+    public static function GET($url)
+    {
+        return HttpRequest::request($url);
+    }
+
+    /*
+     * @param $url 请求地址
+     * @param $data 发送的数据
+     * @return 返回json
+     */
+    public static function POST($url, $data)
+    {
+        return HttpRequest::request($url, 'POST', $data);
+    }
+
+    /*
+     * @param $url 请求地址
+     * @param $data 删除的数据
+     * @return 返回json
+     */
+    public static function DELETE($url, $data)
+    {
+        return HttpRequest::request($url, 'DELETE', $data);
+    }
+
+    /*
+     * @param $url 请求地址
+     * @param $data 修改的数据
+     * @return 返回json
+     */
+    public static function PUT($url, $data)
+    {
+        return HttpRequest::request($url, 'PUT', $data);
+    }
+
+    /*
+     * @param $type 是所需数据的类型  如果为空 则返回所有缓存 如果存在 则返回该类型数据
+     * 该函数判断缓存是都存在 如果存在则直接返回缓存数据 否则返回false
+     */
+    public static function GetDate($type = "")
+    {
+        $id = \Yii::$app->user->getId();
+        $cache = \Yii::$app->cache;
+        if (self::checkChace($type)) {
+            $data = $cache->get($id);
+            if (!empty($type)) {
+                return $data[$type];
+            } else {
+                return $data;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static function checkChace($type = "")
+    {
+        $id = \Yii::$app->user->getId();
+        $cache = \Yii::$app->cache;
+        $data = $cache->get($id);
+        if (!empty($type) && !empty($data[$type])) {
+            return true;
+        } else if (empty($data)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    /*
+     *@param $id 用户id
+     *@param $type 储存的类型
+     */
+    public static function createCache($id, $type, $data)
+    {
+        $cache = \Yii::$app->cache;
+        $data[$type] = $data;
+        $data = $cache->get($id);
+        return $cache->set($id, $data[$type]);
+    }
+
+}
--- frontend/models/Group.php
+++ frontend/models/Group.php
@@ -25,12 +25,38 @@ class Group extends ActiveRecord
     {
         return [
             'id' => 'ID',
-            'user_id'=>'创建人',
-            'name'=>'团队名',
+            'user_id' => '创建人',
+            'name' => '团队名',
             'desc' => '描述',
             'created_time' => '组织创建时间',
         ];
     }
 
+    /*
+     * @param $type 返回数据类型是数组还是对象
+     * @return 返回一个数组 键名 array object
+     */
+    public static function GetAllGroup()
+    {
+        $user_id = \Yii::$app->user->getId();
+        $data_json = HttpRequest::request($user_id . '?type=user');
+        $data_array = json_decode($data_json, true);
+        $data_object = json_decode($data_json);
+        $data=array(
+            'array'=>$data_array,
+            'object'=>$data_object
+            );
+        return $data;
+
+    }
+
+    /*
+     * @param $gid 组织id
+     */
+
+    public static function GetOneGroup($gid)
+    {
+
+    }
 
 }
